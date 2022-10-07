b3a6938446190c84c456d9469cd69ac3f8a19f27
Aakash <vermaaakash1000@gmail.com>
2022-10-07 23:30:04 +0530

    Manual Header Added

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -7,8 +7,56 @@ export default function ManualHeader() {
     isWeb3Enabled,
     isWeb3EnableLoading,
     Moralis,
-    deactivateMoralis,
+    deactivateWeb3,
   } = useMoralis();
 
-  return <div>Hi there</div>;
+  useEffect(() => {
+    if (
+      !isWeb3Enabled &&
+      typeof window !== "undefined" &&
+      window.localStorage.getItem("connected")
+    ) {
+      enableWeb3();
+    }
+  }, [isWeb3Enabled]);
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      console.log(`Account Changed to ${account}`);
+      if (account == null) {
+        window.localStorage.removeItem("connected");
+        deactivateWeb3();
+        console.log("Null Account Found");
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      {account ? (
+        <div className="ml-auto py-2 px-4">
+          Connected to {account.slice(0, 6)}...
+          {account.slice(account.length - 4)}
+        </div>
+      ) : (
+        <button
+          onClick={async () => {
+            // await walletModal.connect()
+            const ret = await enableWeb3();
+            if (typeof ret !== "undefined") {
+              // depends on what button they picked
+              if (typeof window !== "undefined") {
+                window.localStorage.setItem("connected", "injected");
+                // window.localStorage.setItem("connected", "walletconnect")
+              }
+            }
+          }}
+          disabled={isWeb3EnableLoading}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+        >
+          Connect
+        </button>
+      )}
+    </div>
+  );
 }
