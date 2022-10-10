f0b628e8439de359be1a5f7edbc7a74750831bca
Aakash <vermaaakash1000@gmail.com>
2022-10-10 23:17:00 +0530

    Header Edited

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import { ConnectButton } from "web3uikit";
 export default function Header() {
   return (
     <div>
+      <h1>Decentralised Lottery</h1>
       <ConnectButton moralisAuth={false} />
     </div>
   );
