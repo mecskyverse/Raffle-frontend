ce2330fc7e7221bd5e34ff3bf0af7e305697c6c9
Aakash <vermaaakash1000@gmail.com>
2022-10-11 17:29:10 +0530

    abi and contractaddress exported

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+const { abi } = require("./contractAbi.json");
+const { contractAddress } = require("./contractAddress.json");
+// console.log(abi);
+module.exports = {
+  contractAddress,
+  abi,
+};
