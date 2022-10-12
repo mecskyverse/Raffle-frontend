a8e0ffc9a090d0aa391500cf5742ceb474c628e8
Aakash <vermaaakash1000@gmail.com>
2022-10-12 16:10:23 +0530

    Entrance Fee Ui added

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,23 +1,24 @@
 import { useWeb3Contract } from "react-moralis";
 import { abi, contractAddress } from "../constants/index";
 import { useMoralis } from "react-moralis";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 export default function LotteryEntrance() {
   // here we are importing an chaindid object and renaming it to chainIdHex
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const [entranceFee, setEntranceFee] = useState("0");
   const chainId = parseInt(chainIdHex);
-  console.log(`contract address = ${contractAddress}`);
 
   const raffleAddress =
     chainId in contractAddress ? contractAddress[chainId][0] : null;
 
-  // const { runContractFunction: enterRaffle } = useWeb3Contract({
-  //   abi: abi,
-  //   contractAddress: raffleAddress,
-  //   functionName: "enterRaffle",
-  //   params: {},
-  //  msg.value,
-  // });
+  const { runContractFunction: enterRaffle } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "enterRaffle",
+    params: {},
+    msgValue: entranceFee,
+  });
 
   const { runContractFunction: getEntranceFee } = useWeb3Contract({
     abi: abi,
@@ -29,11 +30,18 @@ export default function LotteryEntrance() {
   useEffect(() => {
     if (isWeb3Enabled) {
       async function updateUI() {
-        const something = await getEntranceFee();
-        console.log(something);
+        let entranceFeeFromContract = await getEntranceFee();
+
+        setEntranceFee(entranceFeeFromContract);
       }
       updateUI();
     }
   }, [isWeb3Enabled]);
-  return <div>chain Id = {chainId}</div>;
+  return (
+    <div>
+      <button>Enter Raffle</button>
+      <br />
+      Entrance Fee is : {ethers.utils.formatUnits(entranceFee, "ether")}
+    </div>
+  );
 }
