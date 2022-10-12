6412690591ccbd7010c3ce1e8eb45cfd20ba8572
Aakash <vermaaakash1000@gmail.com>
2022-10-12 14:55:50 +0530

    Error correction for abi import

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
-const { abi } = require("./contractAbi.json");
-const { contractAddress } = require("./contractAddress.json");
+const abi = require("./contractAbi.json");
+const contractAddress = require("./contractAddress.json");
 // console.log(abi);
 module.exports = {
   contractAddress,
