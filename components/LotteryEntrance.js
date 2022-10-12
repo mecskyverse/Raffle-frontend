2fb279a5c3a09747b0dae2bede03cb243e211f32
Aakash <vermaaakash1000@gmail.com>
2022-10-12 17:48:59 +0530

    Updated LotteryEntrance

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -3,6 +3,7 @@ import { abi, contractAddress } from "../constants/index";
 import { useMoralis } from "react-moralis";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
 export default function LotteryEntrance() {
   // here we are importing an chaindid object and renaming it to chainIdHex
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
@@ -11,7 +12,7 @@ export default function LotteryEntrance() {
 
   const raffleAddress =
     chainId in contractAddress ? contractAddress[chainId][0] : null;
-
+  const dispatch = useNotification;
   const { runContractFunction: enterRaffle } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress,
@@ -37,11 +38,30 @@ export default function LotteryEntrance() {
       updateUI();
     }
   }, [isWeb3Enabled]);
+  const handleSuccess = async function (tx) {
+    await tx.wait(1);
+    handleNewNotification(tx);
+  };
   return (
     <div>
-      <button>Enter Raffle</button>
-      <br />
-      Entrance Fee is : {ethers.utils.formatUnits(entranceFee, "ether")}
+      Hi From Lottery Entrance!
+      {raffleAddress ? (
+        <div>
+          <button
+            onClick={async () => {
+              await enterRaffle({
+                onSuccess: handleSuccess,
+                onError: (error) => console.log(error),
+              });
+            }}
+          >
+            Enter Raffle
+          </button>
+          Entrance Fee is : {ethers.utils.formatUnits(entranceFee, "ether")}
+        </div>
+      ) : (
+        <div>Contract Not Detected</div>
+      )}
     </div>
   );
 }
