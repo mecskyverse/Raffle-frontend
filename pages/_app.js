356ca7bd35f3b610a961d5219bc99845f6f2c1a2
Aakash <vermaaakash1000@gmail.com>
2022-10-12 17:49:12 +0530

    Notification Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
+import { Notificationprovider } from "web3uikit"
 
 function MyApp({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
-            <Component {...pageProps} />
+            <Notificationprovider>
+                <Component {...pageProps} />
+            </Notificationprovider>
         </MoralisProvider>
     )
 }
